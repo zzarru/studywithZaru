3d6dbc4cfb013a7e7eb675f3917149cd0935d15f
zzarru <sn20180301@gmail.com>
2023-03-22 17:47:52 +0900

    '23 0322 django

--- 04. Django/mypjt/articles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('qwer/', views.hahaha, name='qwer'), #함수 greeting을 불러올 것이다.
-    path('asdf/', views.hohoho, name='asdf'),
-]
--- 04. Django/mypjt/pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def zxcv(request):
-    return render(request, 'pages/zxcv.html')
